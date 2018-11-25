88c45a1850188574de51fbc7c2f9f301085716f8
{name} <{emailaddress}>
2018-11-25 17:17:18 -0200

    aqui coloco uma mensagem, tipo: comitando primeiro post

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host : 'mysql.iluminarsistemas.com.br',
+    user :  'iluminarsistem02',
+    password : 'p4r3q3j2',
+    database : 'iluminarsistem02'  
+});
+
+
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+      });
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
